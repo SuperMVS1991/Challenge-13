cc1f5d79b2ce19e35c7ff69bbf7e868a6de95c1a
SuperMVS1991 <150755455+SuperMVS1991@users.noreply.github.com>
2024-02-29 16:38:22 -0500

    pushing code to repository with category-routs.js code written

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,13 +3,47 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
+router.get('/', async (req, res) => {
+    try {
+      const catagoryData = await Category.findAll({
+        attributes: ['id', 'category_name'],
+        include: [
+          {
+            model: Product,
+            attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+          },
+        ],
+      });
+      res.status(200).json(catagoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
   // be sure to include its associated Products
-});
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
+  try {
+    const catagoryData = await Category.findByPk(req.params.id, {
+      // JOIN with travellers, using the Trip through table
+      attributes: ['id', 'category_name'],
+      include: [
+        {
+          model: Product,
+          attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+        },
+      ],
+    });
+
+    if (!catagoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(catagoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
