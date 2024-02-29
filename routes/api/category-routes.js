3a395d616ec523d65065879f35a9de3edc902c30
SuperMVS1991 <150755455+SuperMVS1991@users.noreply.github.com>
2024-02-29 17:00:59 -0500

    pushing code to repository with completing the category-routes.js file part

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,19 +44,60 @@ router.get('/:id', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-  // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
-});
+    try {
+      const catagoryData = await Category.create(req.body);
+      res.status(200).json(catagoryData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  // Add function body here
   // update a category by its `id` value
-});
+    // update a category by its `id` value
+    try {
+      const catagoryData = await Category.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (!catagoryData[0]) {
+        res.status(404).json({ message: 'No category found with this id!' });
+        return;
+      }
+      res.status(200).json(catagoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+);
 
-router.delete('/:id', (req, res) => {
+
+
+
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
-});
+    // delete a category by its `id` value
+    try {
+      const catagoryData = await Category.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (!catagoryData) {
+        res.status(404).json({ message: 'No category found with this id!' });
+        return;
+      }
+      res.status(200).json(catagoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+      
+  });  
 
 module.exports = router;
